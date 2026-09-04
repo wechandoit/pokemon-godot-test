d985258b18cc5b95cb930ee3cc6eb28aff780ac3
wechandoit <ethanchan7981@gmail.com>
2026-09-04 18:41:21 -0400

    Add slopes

--- OverworldObject2D.gd
+++ OverworldObject2D.gd
@@ -50,7 +50,6 @@ func _ready():
 	play_anim(str("Idle", dirFacing))
 
 func _process(delta):
-	
 	# 1. Unified movement speed calculation
 	var running_input = Input.is_action_pressed("overworld_run")
 	moveVel = runSpeed if running_input else walkSpeed
@@ -58,6 +57,8 @@ func _process(delta):
 	# 2. Prevent single-frame texture swapping between grid tiles
 	var is_moving = moveTimer > 0 or hasPlayerInvokedMove()
 	var is_running = running_input and is_moving
+	
+	print("Player Y -> Tile: %d | World: %.2f" % [posTile.y, position.y])
 	
 	var target_tex = TEX_RUN if is_running else TEX_WALK
 	if sprite.texture != target_tex:
@@ -69,9 +70,11 @@ func _process(delta):
 		play_anim(str(anim_prefix, dirFacing))
 		
 		position.x = lerp(posTile.x, posTileLast.x, moveTimer) * world.cell_size.x
+		position.y = lerp(posTile.y, posTileLast.y, moveTimer) * world.cell_size.y
 		position.z = lerp(posTile.z, posTileLast.z, moveTimer) * world.cell_size.z
 	else:
 		position.x = posTile.x * world.cell_size.x
+		position.y = posTile.y * world.cell_size.y
 		position.z = posTile.z * world.cell_size.z
 		
 		if hasPlayerInvokedMove():
@@ -99,20 +102,44 @@ func _process(delta):
 				if not isFacingTileSolid():
 					posTileLast = posTile
 					posTile += FACING_TO_OFFSET[dirFacing]
-					var celId = world.get_cell_item(Vector3i(posTile))
 					slopeSpeedMultiplier = 1.0
 					
-					if celId != GridMap.INVALID_CELL_ITEM and tileDefs.collisionType[celId] == GridmapCollisionHelper.TYPES.SLOPE:
-						posTile.y += 1
+					var uphillOri = GridmapCollisionHelper.ORTHO_TO_INDEX[FACING_INVERSE[dirFacing]]
+					var downhillOri = GridmapCollisionHelper.ORTHO_TO_INDEX[dirFacing]
+
+					# UPHILL STEP 1: Stepping off a slope onto an elevated flat floor
+					var lastFloorTile = Vector3i(posTileLast.x, posTileLast.y - 1, posTileLast.z)
+					var lastCelId = world.get_cell_item(lastFloorTile)
+					var targetTopFloorTile = Vector3i(posTile.x, posTile.y, posTile.z)
+					var targetTopCelId = world.get_cell_item(targetTopFloorTile)
+
+					if lastCelId != GridMap.INVALID_CELL_ITEM \
+					and tileDefs.collisionType[lastCelId] == GridmapCollisionHelper.TYPES.SLOPE \
+					and world.get_cell_item_orientation(lastFloorTile) == uphillOri:
+						if targetTopCelId != GridMap.INVALID_CELL_ITEM \
+						and tileDefs.collisionType[targetTopCelId] != GridmapCollisionHelper.TYPES.SLOPE:
+							posTile.y += 1
+							slopeSpeedMultiplier = 0.7
+
+					# UPHILL STEP 2: Stepping onto a slope tile from ground level
+					var targetFloor = Vector3i(posTile.x, posTile.y - 1, posTile.z)
+					var celId = world.get_cell_item(targetFloor)
+					if celId != GridMap.INVALID_CELL_ITEM \
+					and tileDefs.collisionType[celId] == GridmapCollisionHelper.TYPES.SLOPE \
+					and world.get_cell_item_orientation(targetFloor) == uphillOri:
 						slopeSpeedMultiplier = 0.7
-						
-					var lastPosFloor = Vector3i(posTileLast.x, posTileLast.y - 1, posTileLast.z)
-					celId = world.get_cell_item(lastPosFloor)
-					
-					if celId != GridMap.INVALID_CELL_ITEM:
-						if GridmapCollisionHelper.ORTHO_TO_INDEX[dirFacing] == world.get_cell_item_orientation(lastPosFloor) and tileDefs.collisionType[celId] == GridmapCollisionHelper.TYPES.SLOPE:
+
+					# DOWNHILL STEP: Stepping down onto a slope tile
+					if posTile.y > 1:
+						var targetSlopeBelow = Vector3i(posTile.x, posTile.y - 2, posTile.z)
+						var targetBelowCelId = world.get_cell_item(targetSlopeBelow)
+
+						if targetBelowCelId != GridMap.INVALID_CELL_ITEM \
+						and tileDefs.collisionType[targetBelowCelId] == GridmapCollisionHelper.TYPES.SLOPE \
+						and world.get_cell_item_orientation(targetSlopeBelow) == downhillOri:
 							posTile.y -= 1
 							slopeSpeedMultiplier = 0.85
+
 					moveTimer = 1.0
 					var anim_prefix = "Run" if is_running else "Walk"
 					play_anim(str(anim_prefix, dirFacing))
@@ -132,7 +159,6 @@ func _process(delta):
 	quickTurnTimer -= delta
 	lastDirFacing = dirFacing
 
-# Helper function to prevent restarting the animation track every frame
 func play_anim(anim_name: String):
 	if animPlayer.current_animation != anim_name:
 		animPlayer.play(anim_name)
@@ -141,42 +167,62 @@ func isFacingTileSolid() -> bool:
 	if not tileDefs: 
 		return true
 	
-	var standingCel = world.get_cell_item(Vector3i(posTile.x, posTile.y - 1, posTile.z))
 	var checkTile = posTile + FACING_TO_OFFSET[dirFacing]
-	var waistCelId = world.get_cell_item(Vector3i(checkTile))
-	var floorCheckTile = Vector3i(checkTile.x, checkTile.y - 1, checkTile.z)
 	
-	if waistCelId != GridMap.INVALID_CELL_ITEM:
-		match tileDefs.collisionType[waistCelId]:
-			GridmapCollisionHelper.TYPES.SOLID:
-				return true
-			GridmapCollisionHelper.TYPES.SLOPE:
-				if GridmapCollisionHelper.ORTHO_TO_INDEX[FACING_INVERSE[dirFacing]] != world.get_cell_item_orientation(Vector3i(checkTile)):
-					return true
-	
+	# Check if player is currently standing on a slope
+	var standingFloor = Vector3i(posTile.x, posTile.y - 1, posTile.z)
+	var standingCel = world.get_cell_item(standingFloor)
+	var standingOnSlope = (standingCel != GridMap.INVALID_CELL_ITEM and tileDefs.collisionType[standingCel] == GridmapCollisionHelper.TYPES.SLOPE)
+	var standingSlopeOri = world.get_cell_item_orientation(standingFloor) if standingOnSlope else -1
+
+	var uphillOri = GridmapCollisionHelper.ORTHO_TO_INDEX[FACING_INVERSE[dirFacing]]
+	var downhillOri = GridmapCollisionHelper.ORTHO_TO_INDEX[dirFacing]
+
+	# 1. Check elevated target tile at posTile.y (stepping uphill off slope onto flat floor)
+	var targetAtY = Vector3i(checkTile.x, posTile.y, checkTile.z)
+	var celAtY = world.get_cell_item(targetAtY)
+	if standingOnSlope and standingSlopeOri == uphillOri and celAtY != GridMap.INVALID_CELL_ITEM:
+		if tileDefs.collisionType[celAtY] != GridmapCollisionHelper.TYPES.SOLID:
+			return false # Allowed to step up onto elevated flat floor
+
+	# 2. Check head/waist height block at current elevated target
+	var waistCelId = world.get_cell_item(Vector3i(checkTile.x, posTile.y, checkTile.z))
+	if waistCelId != GridMap.INVALID_CELL_ITEM and tileDefs.collisionType[waistCelId] == GridmapCollisionHelper.TYPES.SOLID:
+		return true
+
+	# 3. Check floor layer directly ahead at y - 1
+	var floorCheckTile = Vector3i(checkTile.x, posTile.y - 1, checkTile.z)
 	var celId = world.get_cell_item(floorCheckTile)
-	var celOri = world.get_cell_item_orientation(floorCheckTile)
-	var testOri = world.get_cell_item_orientation(Vector3i(posTile.x, posTile.y - 1, posTile.z))
-	
-	if standingCel != GridMap.INVALID_CELL_ITEM and tileDefs.collisionType[standingCel] == GridmapCollisionHelper.TYPES.SLOPE:
-		if celId != GridMap.INVALID_CELL_ITEM and tileDefs.collisionType[celId] == GridmapCollisionHelper.TYPES.SLOPE and testOri == celOri:
-			return false
-		if GridmapCollisionHelper.ORTHO_TO_INDEX[dirFacing] != testOri and GridmapCollisionHelper.ORTHO_TO_INDEX[FACING_INVERSE[dirFacing]] != testOri:
+
+	if celId != GridMap.INVALID_CELL_ITEM:
+		var celType = tileDefs.collisionType[celId]
+		if celType == GridmapCollisionHelper.TYPES.SOLID:
 			return true
-			
-	if celId != GridMap.INVALID_CELL_ITEM and tileDefs.collisionType[celId] == GridmapCollisionHelper.TYPES.SLOPE and GridmapCollisionHelper.ORTHO_TO_INDEX[dirFacing] != celOri:
-		if GridmapCollisionHelper.INDEX_IS_SLOPE[celOri]:
+		elif celType == GridmapCollisionHelper.TYPES.SLOPE:
+			var celOri = world.get_cell_item_orientation(floorCheckTile)
+			# Uphill step onto slope
+			if celOri == uphillOri:
+				return false
+			# Parallel/sideways step on slope
+			if standingOnSlope and celOri == standingSlopeOri:
+				return false
 			return true
-		return false
-		
-	var celIdForSlope = world.get_cell_item(Vector3i(checkTile.x, checkTile.y - 2, checkTile.z))
-	if celId == GridMap.INVALID_CELL_ITEM:
-		if standingCel != GridMap.INVALID_CELL_ITEM and tileDefs.collisionType[standingCel] == GridmapCollisionHelper.TYPES.SLOPE and GridmapCollisionHelper.ORTHO_TO_INDEX[dirFacing] == testOri and celIdForSlope != GridMap.INVALID_CELL_ITEM:
-			return false
 		else:
-			return true
-			
-	return false
+			# Normal flat floor ahead at same layer
+			return false
+
+	# 4. Check layer below at y - 2 (stepping downhill onto slope or parallel slope)
+	var slopeBelow = Vector3i(checkTile.x, posTile.y - 2, checkTile.z)
+	var celIdBelow = world.get_cell_item(slopeBelow)
+	if celIdBelow != GridMap.INVALID_CELL_ITEM:
+		var celTypeBelow = tileDefs.collisionType[celIdBelow]
+		if celTypeBelow == GridmapCollisionHelper.TYPES.SLOPE:
+			var slopeOri = world.get_cell_item_orientation(slopeBelow)
+			if slopeOri == downhillOri or (standingOnSlope and slopeOri == standingSlopeOri):
+				return false
+
+	# Block ledges/cliffs without slopes
+	return true
 
 func hasPlayerInvokedMove() -> bool:
 	return Input.is_action_pressed("overworld_up") or Input.is_action_pressed("overworld_down") or Input.is_action_pressed("overworld_left") or Input.is_action_pressed("overworld_right")
